b666fc67b9462faae1a301414bb8d675b4d4380d
Aditya Mishra <aditya@Adityas-MacBook-Air.local>
2026-02-18 01:45:46 +0530

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,13 @@
+import HeroSection from "@/components/home/HeroSection";
 export default function HomePage() {
   return (
     <div>
-
-      <section className="py-20 text-center">
-        <h1 className="text-5xl font-bold mb-4">
-          Book Your Vehicle Easily
-        </h1>
-
-        <p className="text-gray-600">
-          Fast, reliable and affordable vehicle booking service.
-        </p>
-
-        <button className="mt-6 bg-blue-600 text-white px-6 py-2 rounded">
+      <div className="text-center">
+      <HeroSection />
+        <button className="bg-blue-600 text-white px-6 py-2 rounded">
           Book Now
         </button>
-      </section>
-
+      </div>
     </div>
   );
 }
